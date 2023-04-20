25d6dbb1df5ff9229d96d1cfbdea5144da9058f3
aakash013srivastava <aakash013srivastava@gmail.com>
2023-04-20 09:56:24 +0530

    access key for api removed

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const searchImages = async(term)=>{
     const response = await axios.get('https://api.unsplash.com/search/photos',{
         headers:{
-            Authorization: 'Client-ID ICRBu9PB7r_Rae849uVys1k4YmsC7BG6N5p-OAQDdXI'
+            Authorization: 'Client-ID access-key-here'
         },
         params:{
             query: term
